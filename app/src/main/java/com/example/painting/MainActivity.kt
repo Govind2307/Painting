0494877ab6d9e0a148d1dda3c13c146cbcb48fbf
hp <govindpurohit@gmail.com>
2022-03-17 19:14:54 +0530

    Toolbar Working

--- app/src/main/java/com/example/painting/MainActivity.kt
+++ app/src/main/java/com/example/painting/MainActivity.kt
@@ -6,11 +6,13 @@ import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.AdapterListUpdateCallback
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +24,7 @@ class MainActivity : AppCompatActivity() {
             "https://surface678.com/wp-content/uploads/elementor/thumbs/edited_wake_tech-ovagsl83z9l43ggkcb0jjyd5vo1na0fp5ta32k5qwg.jpg",
             "Leonardo",
             "popular",
-            "writer"
+            "he medium is commonly applied to the base with a brush, but other implements, such as knives, sponges, and airbrushes, can be used."
         ),
         Abc(
             "https://surface678.com/wp-content/uploads/elementor/thumbs/edited_wake_tech-ovagsl83z9l43ggkcb0jjyd5vo1na0fp5ta32k5qwg.jpg",
@@ -48,12 +50,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener{
+            Toast.makeText(this,"Navigation",Toast.LENGTH_LONG).show()
+        }
         up = findViewById(R.id.up)
         up.setOnClickListener{
             recycler.smoothScrollToPosition(0)
 
     }
         initView();
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mainmenu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var itemview = item.itemId
+        when(itemview){
+            R.id.search -> Toast.makeText(applicationContext,"Search",Toast.LENGTH_LONG).show()
+            R.id.menu -> Toast.makeText(applicationContext,"menu",Toast.LENGTH_LONG).show()
+        }
+        return false
     }
 
 
